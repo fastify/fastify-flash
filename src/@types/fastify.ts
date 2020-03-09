c57b325a68a2a7e2dd33afe33f34594c98d57ef7
Maksim Sinik <maksim@sinik.it>
2020-03-09 18:25:16 +0100

    Fixes flash messages not being passed between requests

--- src/@types/fastify.ts
+++ src/@types/fastify.ts
@@ -3,9 +3,9 @@ import { flashFactory } from '../flash'
 declare module 'fastify' {
   export interface FastifyRequest<HttpRequest, Query, Params, Headers, Body> {
     session: {
-      flash: {
-        [k: string]: string[]
-      }
+      [key: string]: any
+      get(key: string): any
+      set(key: string, value: any): void
     }
     flash: ReturnType<typeof flashFactory>['request']
   }
--- src/flash.ts
+++ src/flash.ts
@@ -1,43 +1,54 @@
 import { format } from 'util'
 
-export function flashFactory() {
-  let currentSession: { flash: { [k: string]: undefined | string[] } }
+type ReplyReturn =
+  | {
+      [k: string]: string[] | undefined
+    }
+  | string[]
 
+export function flashFactory() {
   return {
     request(type: string, ...message: string[] | [string[]]): number {
+      let currentSession = this.session.get('flash')
+      if (!currentSession) {
+        currentSession = {}
+        this.session.set('flash', currentSession)
+      }
+
       if (message.length === 0) {
         throw new Error('Provide a message to flash.')
       }
+
       if (Array.isArray(message[0])) {
         for (let i = 0; i < message[0].length; i++) {
-          ;(currentSession.flash[type] = currentSession.flash[type] || []).push(message[0][i])
+          currentSession = {
+            ...currentSession,
+            [type]: (currentSession[type] || []).concat(message[0][i]),
+          }
         }
-        return currentSession.flash[type]!.length
+      } else {
+        currentSession = {
+          ...currentSession,
+          [type]: (currentSession[type] || []).concat(
+            message.length > 1 ? format.apply(undefined, message) : message[0],
+          ),
+        }
       }
-
-      ;(currentSession.flash[type] = currentSession.flash[type] || []).push(
-        message.length > 1 ? format.apply(undefined, message) : message[0],
-      )
-
-      return currentSession.flash[type]!.length
+      this.session.set('flash', currentSession)
+      return this.session.get('flash')[type].length
     },
 
-    reply(type?: string): typeof currentSession.flash | string[] {
+    reply(type?: string): ReplyReturn {
       if (!type) {
-        const allMessages = currentSession.flash
-        currentSession.flash = {}
+        const allMessages = this.request.session.get('flash')
+        this.request.session.set('flash', {})
         return allMessages
       }
 
-      const messages = currentSession.flash[type]
-      currentSession.flash[type] = undefined
+      const { [type]: messages, ...flash } = this.request.session.get('flash') || {}
+      this.request.session.set('flash', flash)
 
       return messages || []
-    },
-
-    setSession(session: any) {
-      currentSession = session
-      currentSession.flash = currentSession.flash || {}
     },
   }
 }
