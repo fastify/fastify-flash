45384ebac3e96b8afb4609851628a66434a85b37
uzlopak <aras.abbasi@googlemail.com>
2022-06-21 14:45:46 +0200

    improve code-coverage

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import fp from 'fastify-plugin'
 import { flashFactory } from './flash'
 
 export = fp<{}>(
-  function (fastify, opts = {}, done) {
+  function (fastify, opts, done) {
     const flash = flashFactory()
 
     fastify.decorateRequest('flash', flash.request)
