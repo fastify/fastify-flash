d43e8f85e85cb4623fd2363bd2d03a5d586ac767
narendra-reddy-nf <narendra.reddy@nearform.com>
2024-09-12 16:51:46 +0530

    chore: migrate to node:test with c8 (#110)
    * migrated tests to node:test using borp
    
    * fixed linting issues
    
    * removed test reporter from the code

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,4 @@
-import { test } from 'tap'
+import { test, TestContext } from 'node:test'
 import { join } from 'path'
 import { readFileSync } from 'fs'
 import Fastify from 'fastify'
@@ -6,9 +6,9 @@ import fastifySession from '@fastify/secure-session'
 import querystring from 'querystring'
 import fastifyFlash from '../src'
 
-const key = readFileSync(join(__dirname, '..', 'secret-key'))
+const key = readFileSync(join(__dirname, '..', '..', 'secret-key'))
 
-test('should set error message and and clear up after displaying.', async t => {
+test('should set error message and and clear up after displaying.', async (t: TestContext) => {
   t.plan(5)
   const fastify = Fastify()
 
@@ -21,9 +21,9 @@ test('should set error message and and clear up after displaying.', async t => {
   fastify.get('/test', (req, reply) => {
     const count = req.flash('error', 'Something went wrong')
 
-    t.equal(count, 1)
-    t.equal(Object.keys(req.session.get('flash')).length, 1)
-    t.equal(req.session.get('flash').error.length, 1)
+    t.assert.strictEqual(count, 1)
+    t.assert.strictEqual(Object.keys(req.session.get('flash')).length, 1)
+    t.assert.strictEqual(req.session.get('flash').error.length, 1)
     const error = reply.flash('error')
     reply.send({ error })
   })
@@ -32,11 +32,11 @@ test('should set error message and and clear up after displaying.', async t => {
     method: 'GET',
     url: '/test',
   })
-  t.equal(response.payload, '{"error":["Something went wrong"]}')
-  t.equal(response.statusCode, 200)
+  t.assert.strictEqual(response.payload, '{"error":["Something went wrong"]}')
+  t.assert.strictEqual(response.statusCode, 200)
 })
 
-test('should set multiple flash messages.', async t => {
+test('should set multiple flash messages.', async (t: TestContext) => {
   t.plan(5)
   const fastify = Fastify()
 
@@ -49,9 +49,9 @@ test('should set multiple flash messages.', async t => {
     req.flash('info', 'Welcome')
     const count = req.flash('info', 'Check out this great new feature')
 
-    t.equal(count, 2)
-    t.equal(Object.keys(req.session.get('flash')).length, 1)
-    t.equal(req.session.get('flash').info.length, 2)
+    t.assert.strictEqual(count, 2)
+    t.assert.strictEqual(Object.keys(req.session.get('flash')).length, 1)
+    t.assert.strictEqual(req.session.get('flash').info.length, 2)
     const info = reply.flash('info')
 
     reply.send({ info })
@@ -61,11 +61,11 @@ test('should set multiple flash messages.', async t => {
     method: 'GET',
     url: '/test',
   })
-  t.equal(response.payload, '{"info":["Welcome","Check out this great new feature"]}')
-  t.equal(response.statusCode, 200)
+  t.assert.strictEqual(response.payload, '{"info":["Welcome","Check out this great new feature"]}')
+  t.assert.strictEqual(response.statusCode, 200)
 })
 
-test('should set flash messages in one call.', async t => {
+test('should set flash messages in one call.', async (t: TestContext) => {
   t.plan(6)
   const fastify = Fastify()
 
@@ -76,13 +76,13 @@ test('should set flash messages in one call.', async t => {
 
   fastify.get('/test', (req, reply) => {
     const count = req.flash('warning', ['username required', 'password required'])
-    t.equal(count, 2)
+    t.assert.strictEqual(count, 2)
 
     const warning = reply.flash('warning')
-    t.equal(warning.length, 2)
+    t.assert.strictEqual(warning.length, 2)
 
-    t.equal(warning[0], 'username required')
-    t.equal(warning[1], 'password required')
+    t.assert.strictEqual(warning[0], 'username required')
+    t.assert.strictEqual(warning[1], 'password required')
     reply.send({ warning })
   })
 
@@ -90,11 +90,11 @@ test('should set flash messages in one call.', async t => {
     method: 'GET',
     url: '/test',
   })
-  t.equal(response.payload, '{"warning":["username required","password required"]}')
-  t.equal(response.statusCode, 200)
+  t.assert.strictEqual(response.payload, '{"warning":["username required","password required"]}')
+  t.assert.strictEqual(response.statusCode, 200)
 })
 
-test('should pass flash between requests.', async t => {
+test('should pass flash between requests.', async (t: TestContext) => {
   t.plan(8)
   const fastify = Fastify()
 
@@ -105,16 +105,16 @@ test('should pass flash between requests.', async t => {
 
   fastify.get('/test', (req, reply) => {
     const count = req.flash('warning', ['username required', 'password required'])
-    t.equal(count, 2)
+    t.assert.strictEqual(count, 2)
     reply.send({})
   })
 
   fastify.get('/test2', (_, reply) => {
     const warning = reply.flash('warning')
-    t.equal(warning.length, 2)
+    t.assert.strictEqual(warning.length, 2)
 
-    t.equal(warning[0], 'username required')
-    t.equal(warning[1], 'password required')
+    t.assert.strictEqual(warning[0], 'username required')
+    t.assert.strictEqual(warning[1], 'password required')
     reply.send({ warning })
   })
 
@@ -123,8 +123,8 @@ test('should pass flash between requests.', async t => {
     url: '/test',
   })
 
-  t.equal(response.payload, '{}')
-  t.equal(response.statusCode, 200)
+  t.assert.strictEqual(response.payload, '{}')
+  t.assert.strictEqual(response.statusCode, 200)
 
   const response2 = await (fastify as any).inject({
     method: 'GET',
@@ -135,11 +135,11 @@ test('should pass flash between requests.', async t => {
       ),
     },
   })
-  t.equal(response2.payload, '{"warning":["username required","password required"]}')
-  t.equal(response2.statusCode, 200)
+  t.assert.strictEqual(response2.payload, '{"warning":["username required","password required"]}')
+  t.assert.strictEqual(response2.statusCode, 200)
 })
 
-test('should pass flash between requests. / 2', async t => {
+test('should pass flash between requests. / 2', async (t: TestContext) => {
   t.plan(8)
   const fastify = Fastify()
 
@@ -151,16 +151,16 @@ test('should pass flash between requests. / 2', async t => {
   fastify.get('/test', (req, reply) => {
     const count = req.flash('warning', ['username required', 'password required'])
     req.flash('info', 'Welcome')
-    t.equal(count, 2)
+    t.assert.strictEqual(count, 2)
     reply.send({})
   })
 
   fastify.get('/test2', (_, reply) => {
     const warning = reply.flash('warning')
-    t.equal(warning.length, 2)
+    t.assert.strictEqual(warning.length, 2)
 
-    t.equal(warning[0], 'username required')
-    t.equal(warning[1], 'password required')
+    t.assert.strictEqual(warning[0], 'username required')
+    t.assert.strictEqual(warning[1], 'password required')
     reply.send({ warning })
   })
 
@@ -169,8 +169,8 @@ test('should pass flash between requests. / 2', async t => {
     url: '/test',
   })
 
-  t.equal(response.payload, '{}')
-  t.equal(response.statusCode, 200)
+  t.assert.strictEqual(response.payload, '{}')
+  t.assert.strictEqual(response.statusCode, 200)
 
   const response2 = await (fastify as any).inject({
     method: 'GET',
@@ -181,11 +181,11 @@ test('should pass flash between requests. / 2', async t => {
       ),
     },
   })
-  t.equal(response2.payload, '{"warning":["username required","password required"]}')
-  t.equal(response2.statusCode, 200)
+  t.assert.strictEqual(response2.payload, '{"warning":["username required","password required"]}')
+  t.assert.strictEqual(response2.statusCode, 200)
 })
 
-test('should independently set, get and clear flash messages of multiple types.', async t => {
+test('should independently set, get and clear flash messages of multiple types.', async (t: TestContext) => {
   t.plan(9)
   const fastify = Fastify()
 
@@ -198,17 +198,17 @@ test('should independently set, get and clear flash messages of multiple types.'
     req.flash('info', 'Welcome back')
     req.flash('notice', 'Last login was yesterday')
 
-    t.equal(Object.keys(req.session.get('flash')).length, 2)
-    t.equal(req.session.get('flash').info.length, 1)
-    t.equal(req.session.get('flash').notice.length, 1)
+    t.assert.strictEqual(Object.keys(req.session.get('flash')).length, 2)
+    t.assert.strictEqual(req.session.get('flash').info.length, 1)
+    t.assert.strictEqual(req.session.get('flash').notice.length, 1)
 
     const info = reply.flash('info')
-    t.equal(info.length, 1)
-    t.equal(info[0], 'Welcome back')
+    t.assert.strictEqual(info.length, 1)
+    t.assert.strictEqual(info[0], 'Welcome back')
 
     const notice = reply.flash('notice')
-    t.equal(notice.length, 1)
-    t.equal(notice[0], 'Last login was yesterday')
+    t.assert.strictEqual(notice.length, 1)
+    t.assert.strictEqual(notice[0], 'Last login was yesterday')
 
     reply.send({ info, notice })
   })
@@ -217,11 +217,11 @@ test('should independently set, get and clear flash messages of multiple types.'
     method: 'GET',
     url: '/test',
   })
-  t.equal(response.payload, '{"info":["Welcome back"],"notice":["Last login was yesterday"]}')
-  t.equal(response.statusCode, 200)
+  t.assert.strictEqual(response.payload, '{"info":["Welcome back"],"notice":["Last login was yesterday"]}')
+  t.assert.strictEqual(response.statusCode, 200)
 })
 
-test('should return all messages and clear.', async t => {
+test('should return all messages and clear.', async (t: TestContext) => {
   t.plan(5)
   const fastify = Fastify()
 
@@ -236,9 +236,9 @@ test('should return all messages and clear.', async t => {
     req.flash('notice', 'Things are looking bleak')
 
     const msgs = reply.flash()
-    t.equal(Object.keys(msgs).length, 2)
-    t.equal(msgs['error'].length, 2)
-    t.equal(msgs['notice'].length, 1)
+    t.assert.strictEqual(Object.keys(msgs).length, 2)
+    t.assert.strictEqual(msgs['error'].length, 2)
+    t.assert.strictEqual(msgs['notice'].length, 1)
 
     reply.send({ ...msgs })
   })
@@ -247,14 +247,14 @@ test('should return all messages and clear.', async t => {
     method: 'GET',
     url: '/test',
   })
-  t.equal(
+  t.assert.strictEqual(
     response.payload,
     '{"error":["Database is down","Message queue is down"],"notice":["Things are looking bleak"]}',
   )
-  t.equal(response.statusCode, 200)
+  t.assert.strictEqual(response.statusCode, 200)
 })
 
-test('should format messages.', async t => {
+test('should format messages.', async (t: TestContext) => {
   t.plan(4)
   const fastify = Fastify()
 
@@ -266,12 +266,12 @@ test('should format messages.', async t => {
   fastify.get('/test', (req, reply) => {
     req.flash('info', 'Hello %s', 'Jared')
     const jared = reply.flash('info')[0]
-    t.equal(jared, 'Hello Jared')
+    t.assert.strictEqual(jared, 'Hello Jared')
 
     req.flash('info', 'Hello %s %s', 'Jared', 'Hanson')
     const jaredHanson = reply.flash('info')[0]
 
-    t.equal(jaredHanson, 'Hello Jared Hanson')
+    t.assert.strictEqual(jaredHanson, 'Hello Jared Hanson')
 
     reply.send({ jared, jaredHanson })
   })
@@ -280,11 +280,11 @@ test('should format messages.', async t => {
     method: 'GET',
     url: '/test',
   })
-  t.equal(response.payload, '{"jared":"Hello Jared","jaredHanson":"Hello Jared Hanson"}')
-  t.equal(response.statusCode, 200)
+  t.assert.strictEqual(response.payload, '{"jared":"Hello Jared","jaredHanson":"Hello Jared Hanson"}')
+  t.assert.strictEqual(response.statusCode, 200)
 })
 
-test('should return empty array when the type is not set', async t => {
+test('should return empty array when the type is not set', async (t: TestContext) => {
   t.plan(6)
   const fastify = Fastify()
 
@@ -297,10 +297,10 @@ test('should return empty array when the type is not set', async t => {
   fastify.get('/test', (req, reply) => {
     const count = req.flash('info', 'Hello, world!')
 
-    t.equal(count, 1)
-    t.equal(Object.keys(req.session.get('flash')).length, 1)
-    t.equal(req.session.get('flash').info.length, 1)
-    t.equal(req.session.get('flash').warning, undefined)
+    t.assert.strictEqual(count, 1)
+    t.assert.strictEqual(Object.keys(req.session.get('flash')).length, 1)
+    t.assert.strictEqual(req.session.get('flash').info.length, 1)
+    t.assert.strictEqual(req.session.get('flash').warning, undefined)
     const warning = reply.flash('warning')
     reply.send({ warning })
   })
@@ -309,11 +309,11 @@ test('should return empty array when the type is not set', async t => {
     method: 'GET',
     url: '/test',
   })
-  t.equal(response.payload, '{"warning":[]}')
-  t.equal(response.statusCode, 200)
+  t.assert.strictEqual(response.payload, '{"warning":[]}')
+  t.assert.strictEqual(response.statusCode, 200)
 })
 
-test('should throw error when try to set flash without message.', async t => {
+test('should throw error when try to set flash without message.', async (t: TestContext) => {
   t.plan(2)
   const fastify = Fastify()
 
@@ -330,14 +330,14 @@ test('should throw error when try to set flash without message.', async t => {
     method: 'GET',
     url: '/test',
   })
-  t.equal(response.payload, '{"statusCode":500,"error":"Internal Server Error","message":"Provide a message to flash."}')
-  t.equal(response.statusCode, 500)
+  t.assert.strictEqual(response.payload, '{"statusCode":500,"error":"Internal Server Error","message":"Provide a message to flash."}')
+  t.assert.strictEqual(response.statusCode, 500)
 })
 
-test('should return error when session is not defined.', async t => {
+test('should return error when session is not defined.', async (t: TestContext) => {
   t.plan(4)
   const fastify = Fastify()
-  fastify.decorateRequest('session', null)
+  fastify.decorateRequest('session', null as any)
   fastify.register(fastifyFlash)
 
   fastify.get('/test', async (req) => {
@@ -353,8 +353,8 @@ test('should return error when session is not defined.', async t => {
       method: 'GET',
       url: '/test',
     })
-    t.equal(response.payload, '{"statusCode":500,"error":"Internal Server Error","message":"Session not found"}')
-    t.equal(response.statusCode, 500)
+    t.assert.strictEqual(response.payload, '{"statusCode":500,"error":"Internal Server Error","message":"Session not found"}')
+    t.assert.strictEqual(response.statusCode, 500)
   }
 
   {
@@ -362,12 +362,12 @@ test('should return error when session is not defined.', async t => {
       method: 'GET',
       url: '/test2',
     })
-    t.equal(response.payload, '{"statusCode":500,"error":"Internal Server Error","message":"Session not found"}')
-    t.equal(response.statusCode, 500)
+    t.assert.strictEqual(response.payload, '{"statusCode":500,"error":"Internal Server Error","message":"Session not found"}')
+    t.assert.strictEqual(response.statusCode, 500)
   }
 })
 
-test('reply.flash() with no data', async t => {
+test('reply.flash() with no data', async (t: TestContext) => {
   t.plan(2)
   const fastify = Fastify()
 
@@ -386,6 +386,6 @@ test('reply.flash() with no data', async t => {
     method: 'GET',
     url: '/test',
   })
-  t.equal(response.payload, '{"warning":[]}')
-  t.equal(response.statusCode, 200)
+  t.assert.strictEqual(response.payload, '{"warning":[]}')
+  t.assert.strictEqual(response.statusCode, 200)
 })
